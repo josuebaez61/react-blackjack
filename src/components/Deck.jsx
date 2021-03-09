3737dda69d2a69eef718b01a8b24d8be61853b68
Josue <josuebaez61@gmail.com>
2021-03-08 22:55:46 -0300

    fix images

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { pickCard } from '../actions/gameActions';
+import deckImg from '../assets/images/grey_back.png';
 
 const Deck = ({ deck, playerwins }) => {
 
@@ -14,7 +15,7 @@ const Deck = ({ deck, playerwins }) => {
 
     return (
         <div className="game-aside__deck"> 
-            <img onClick={ handleClick } className={ ( (playerwins !== null) || deck.length <= 0 ) ? 'not-allowed img-disabled' : 'pointer' } src={ './assets/images/grey_back.png' } alt="deck"/>
+            <img onClick={ handleClick } className={ ( (playerwins !== null) || deck.length <= 0 ) ? 'not-allowed img-disabled' : 'pointer' } src={ deckImg } alt="deck"/>
             <span className="game-aside__cards-counter" style={ deck.length <= 0 ? { color: 'red'} : { color: 'white'} } >{ deck.length }</span>
         </div>
     )
--- src/components/PlayerCards.jsx
+++ src/components/PlayerCards.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 
 const PlayerCards = ({cardsProp}) => {
-
     const { game: { player_cards, ia_cards, turn } } = useSelector(state => state);
-
+    
     return (
         <div className={ eval(cardsProp).length > 10 ? "in-game-cards in-game-cards--stacked" : "in-game-cards" }>
             {
@@ -14,7 +13,8 @@ const PlayerCards = ({cardsProp}) => {
                 eval(cardsProp).map( card => {
                     // Si la carta invocada es AD, la imagen es bloqueada por los AD block debido al nombre "AD.png", por esta razon se cambio el nombre original por
                     // "A_D.png", asi que controlamos esta excepcion mediante ((card.name === 'AD') ? 'A_D' : card.name)
-                    return (<img key={card.name} src={ './assets/images/' + ((card.name === 'AD') ? 'A_D' : card.name)  + '.png' } alt={card.name} />)
+                    // return (<img key={card.name} src={ './assets/images/' + ((card.name === 'AD') ? 'A_D' : card.name)  + '.png' } alt={card.name} />)
+                    return (<img key={card.name} src={ require('../assets/images/'+ ((card.name === 'AD') ? 'A_D' : card.name)  + '.png').default  } alt={card.name} />)
                 })
             }
         </div>
